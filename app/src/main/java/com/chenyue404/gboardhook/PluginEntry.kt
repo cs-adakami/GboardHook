99b5d48c3faf093e903662d8cc209c0d0c287aea
Bantuan adakami <144202158+cs-adakami@users.noreply.github.com>
2026-04-09 04:25:19 +0700

    Update PluginEntry.kt

--- app/src/main/java/com/chenyue404/gboardhook/PluginEntry.kt
+++ app/src/main/java/com/chenyue404/gboardhook/PluginEntry.kt
@@ -8,8 +8,14 @@ import io.github.libxposed.api.XposedModuleInterface
 class PluginEntry : XposedModule() {
 
     companion object {
+        const val SP_FILE_NAME = "GboardinHook"
+        const val SP_KEY = "key"
+        const val SP_KEY_LOG = "key_log"
+        const val PACKAGE_NAME = "com.google.android.inputmethod.latin"
+        const val DAY: Long = 1000L * 60 * 60 * 24
+        const val DEFAULT_NUM = 10
+        const val DEFAULT_TIME = DAY * 3
         private const val TAG = "GboardHook"
-        private const val PACKAGE_NAME = "com.google.android.inputmethod.latin"
     }
 
     override fun attachFramework(framework: XposedInterface) {
